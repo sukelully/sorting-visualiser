a95db4e4c73d0e7cb045a943562d8c70018ed66c
Luke Sullivan <sully097@gmail.com>
2025-03-10 12:48:48 +0000

    Nearly done, array sorting done

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-// const genBtn = document.getElementById('generate-btn');
-// const sortBtn = document.getElementById('sort-btn');
+const genBtn = document.getElementById('generate-btn');
+const sortBtn = document.getElementById('sort-btn');
+const startingArray = document.getElementById('starting-array');
+const arrayContainer = document.getElementById('array-container');
 
 // Return a random number between 1-100
 const generateElement = () => {
@@ -8,15 +10,16 @@ const generateElement = () => {
 
 // Return an array with 5 random numbers
 const generateArray = () => {
-    return Array.from({ length: 5}, () => generateElement());
+    return Array.from({ length: 5 }, () => generateElement());
 }
 
 // Create and return an empty div
 const generateContainer = () => {
-  return document.createElement('div');
+    return document.createElement('div');
 }
 
-function fillArrContainer(container, arr) {
+// Display array values in the DOM
+const fillArrContainer = (container, arr) => {
     container.innerHTML = '';
 
     for (let i = 0; i < Math.min(5, arr.length); i++) {
@@ -32,16 +35,66 @@ const isOrdered = (a, b) => {
     return (a <= b);
 }
 
+// Swaps two sequential elements in an array if the first is larger than the second
 const swapElements = (arr, i) => {
     if (!isOrdered(arr[i], arr[i + 1])) {
         const temp = arr[i];
         arr[i] = arr[i + 1];
         arr[i + 1] = temp;
-        return `${arr} indexes ${i} and ${i + 1} swapped`;
+        return false;
     } else {
-        return `No elements swapped`;
+        return true;
     }
 }
+
+// Sets a border on the selected elements
+const highlightCurrentEls = (el, i) => {
+    const children = Array.from(el.children);
+
+    children[i].style.border = "dashed 2px red";
+    children[i + 1].style.border = "dashed 2px red";
+}
+
+// Sort elements
+const bubbleSortStep = (arr, container, i = 0, swapped = false) => {
+    // Base case: return when array sorted
+    if (i >= arr.length - 1) {
+        if (!swapped) return;
+
+        // Otherwise, start another full pass from the beginning
+        return setTimeout(() => bubbleSortStep(arr, container, 0, false), 500);
+    }
+
+
+
+    setTimeout(() => {
+        if (!isOrdered(arr[i], arr[i + 1])) {
+            swapElements(arr, i);
+            swapped = true;
+            const nextStep = generateContainer();
+            fillArrContainer(nextStep, arr);
+            highlightCurrentEls(nextStep, i);
+            arrayContainer.append(nextStep);
+        }
+
+        // Move to the next pair
+        bubbleSortStep(arr, container, i + 1, swapped);
+    }, 500);
+};
+
+
+genBtn.addEventListener('click', () => {
+    startingArray.innerHTML = "";
+
+    fillArrContainer(startingArray, generateArray());
+});
+
+sortBtn.addEventListener('click', () => {
+    const arr = Array.from(startingArray.children).map(el => Number(el.textContent));
+
+
+    bubbleSortStep(arr, startingArray);
+});
 
 // const randomElement = generateElement();
 // console.log(randomElement);
@@ -49,10 +102,10 @@ const swapElements = (arr, i) => {
 // const randomArray = generateArray();
 // console.log(randomArray);
 
-// const isOrderedTest = isOrderded(1, 2);
+// const isOrderedTest = isOrdered(1, 2);
 // console.log(isOrderedTest);
 
-const testArr = [1, 2, 3, 4, 5];
+// const testArr = [1, 2, 3, 4, 5];
 
-const swapElementsTest = swapElements(testArr, 0);
-console.log(swapElementsTest);
+// const swapElementsTest = swapElements(testArr, 0);
+// console.log(swapElementsTest);
